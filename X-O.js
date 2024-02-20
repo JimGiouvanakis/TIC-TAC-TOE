87a0ceec9390314e60e907bc13682e0f795a9a17
JimGiouvanakis <106709373+JimGiouvanakis@users.noreply.github.com>
2024-02-21 01:06:58 +0200

    Progress

--- X-O.js
+++ X-O.js
@@ -1,42 +1,89 @@
 const cells = document.querySelectorAll(".cell");
+const content = document.querySelector(".cell").textContent;
 let player = "0";
+let row, row2, row3;
+let col, col2, col3;
+let flag;
 
 function ChangeUser(player) {
-  if (player === `0`) {
+  if (player == `0`) {
     player = `1`;
-  } else if (player === `1`) {
+  } else if (player == `1`) {
     player = `0`;
   }
   return player;
 }
 
-function CheckWinner(row, col) {}
+function CheckWinner(row, col, player) {
+  // Array.from(cells).forEach((cell, index) => {
+  // row2 = Math.floor(index / 3);
+  // col2 = index % 3;
+  flag = 0;
+  if (player == `0`) {
+    if (row == `2`) {
+      Array.from(cells).forEach((cell, index) => {
+        row3 = Math.floor(index / 3);
+        col3 = index % 3;
+        if (row3 == `2` && col3 == `0`) {
+          if (cell.textContent == "X") {
+            flag = `1`;
+          }
+        }
+        if (row3 == `2` && col3 == `1` && flag == `1`) {
+          if (cell.textContent == "X") {
+            flag = `2`;
+          }
+        }
+        if (row3 == `2` && col3 == `2` && flag == `2`) {
+          if (cell.textContent == "X") {
+            flag = `3`;
+          }
+        }
+      });
+    }
+    console.log(`flag`, flag);
+    if (flag == `3`) {
+      console.log(`winner`, player);
+    }
+    // if (row2 === row + 1 && col2 === col + 1) {
+    //   if (cell.classList.contains(`o`)) {
+    //     Array.from(cells).forEach((cell, index) => {
+    //       if (row2 === row + 2 && col2 === col + 2) {
+    //       }
+    //     });
+    //   } else if (cell.classList.contains("x")) {
+    //   }
+    // }
+  }
+  // });
+}
 
 // function CheckCell(cell) {
 //   if (cell.classList.contains("x")) {
 //   }
 // }
-let row;
-let col;
 
 Array.from(cells).forEach((cell, index) => {
-  console.log(cell, cells);
+  // console.log(cell, cells);
   cell.addEventListener("click", function () {
     row = Math.floor(index / 3);
     col = index % 3;
-    if (!cell.classList.contains("x") && !cell.classList.contains("o")) {
-      if (player === `0`) {
-        cell.classList.remove("o");
-        cell.classList.add("x");
+    console.log(row, col);
+    if (!(content == "X") && !(content == "O")) {
+      if (player == `0`) {
+        // cell.classList.remove("o");
+        cell.textContent = "X";
+        // cell.classList.add("x");
+        CheckWinner(row, col, player);
         player = ChangeUser(player);
       } else if (player === `1`) {
-        cell.classList.remove("x");
-        cell.classList.add("o");
+        // cell.classList.remove("x");
+        // cell.classList.add("o");
+        cell.textContent = "O";
+        CheckWinner(row, col, player);
         player = ChangeUser(player);
       }
     }
-    console.log(row, col);
-    CheckWinner(row, col);
-    console.log(CheckWinner(row, col));
+    // console.log(CheckWinner(row, col));
   });
 });
